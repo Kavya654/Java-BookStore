44200c1a9bd0acc84cc93fbdb942bfd7e2d41274
Kavya654 <50830549+Kavya654@users.noreply.github.com>
2019-05-27 19:58:01 +0530

    Update BookDAO.java

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -92,7 +92,7 @@ public class BookDAO {
     }
     
     public void deleteBook(int id) {
-    	String sql = "Delete from book where id = ?";
+    	String sql = "DELETE FROM book WHERE id = ?";
     	
     	try {
     		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
