830442fe1f13f0945ed0ccf433dc419942cf4b0e
kavya654 <kavya.narayan654@gmail.com>
2019-05-29 11:24:42 +0530

    EDIT
    EDIT FORM

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -62,6 +62,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/delete":
 					deleteBook(request, response);
           break;
+				case "/edit":
+					showEditForm(request, response);
+          break;
         default:
 				   listBooks(request, response);
            break;
@@ -72,11 +75,21 @@ public class ControllerServlet extends HttpServlet {
 		}
 	}
 
+	
+
 	private void deleteBook(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		int id =Integer.parseInt(request.getParameter("id"));
 		bookDAO.deleteBook(id);
 		
 		response.sendRedirect("list");
+	}
+	
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)throws ServletException,IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		Book existingBook = bookDAO.getBook(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+		request.setAttribute("book",existingBook );
+		dispatcher.forward(request, response);
 	}
 
 	private void showBookAdmin(HttpServletRequest request, HttpServletResponse response)
